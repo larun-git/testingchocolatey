65c5dcadf0a1148ff6451a7f5d2ab2f44781f151
larun-git <83459772+larun-git@users.noreply.github.com>
2021-08-31 18:58:16 +0530

    Update Perf_test_VM.ps1

--- Perf_test_VM.ps1
+++ Perf_test_VM.ps1
@@ -1,6 +1,9 @@
-﻿# Install Chocolatey
+# Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Software
 choco install jdk8 -y
 choco install jmeter -y
+choco install fiddler -y
+choco install notepadplusplus -y
+choco install chrome-remote-desktop-chrome -y
