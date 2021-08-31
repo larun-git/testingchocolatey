bf1653ef7666a70c68d5d048080af410a6924818
larun-git <83459772+larun-git@users.noreply.github.com>
2021-08-31 15:16:05 +0530

    Delete Perf_test_VM.ps1

--- Perf_test_VM.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Install Software
-choco install jdk8
-choco install jmeter
