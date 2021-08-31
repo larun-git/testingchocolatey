78a44101175a07b38ecdc3cace14f0fae0808ee2
larun-git <83459772+larun-git@users.noreply.github.com>
2021-08-31 15:16:22 +0530

    Add files via upload

---
+++ Perf_test_VM.ps1
@@ -0,0 +1,6 @@
+﻿# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Software
+choco install jdk8 -y
+choco install jmeter -y
