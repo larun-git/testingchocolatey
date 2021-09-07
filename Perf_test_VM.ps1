35e7e03c7eb91e0b0f98a971e737db62291d8dba
larun-git <83459772+larun-git@users.noreply.github.com>
2021-09-07 09:41:44 +0530

    Update Perf_test_VM.ps1

--- Perf_test_VM.ps1
+++ Perf_test_VM.ps1
@@ -4,6 +4,3 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 # Install Software
 choco install jdk8 -y
 choco install jmeter -y
-choco install fiddler -y
-choco install notepadplusplus -y
-choco install chrome-remote-desktop-chrome -y
