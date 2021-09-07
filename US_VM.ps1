17f8897744d5afd6ec3b789a74b83e0091d8752e
larun-git <83459772+larun-git@users.noreply.github.com>
2021-09-07 09:42:38 +0530

    Add files via upload

---
+++ US_VM.ps1
@@ -0,0 +1,7 @@
+﻿# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Software
+choco install fiddler -y
+choco install notepadplusplus -y
+choco install chrome-remote-desktop-chrome -y
