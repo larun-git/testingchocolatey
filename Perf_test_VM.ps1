83d5c89e26152cdb01f869d3ca8887b47eb71ab7
larun-git <83459772+larun-git@users.noreply.github.com>
2021-09-08 17:45:07 +0530

    Update Perf_test_VM.ps1

--- Perf_test_VM.ps1
+++ Perf_test_VM.ps1
@@ -1,3 +1,6 @@
+#Force TLS1.2 connection
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
